e998c66a80b55fadb1e8f6687c4b459dbc9b1b16
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-19 14:49:32 -0400

    Extended the basic server to serve static files.

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func staticFiles(pWriter http.ResponseWriter, pRequest *http.Request) {
+    fileContent, error := ioutil.ReadFile("../frontend/" + pRequest.URL.RequestURI())
+    if error != nil {
+        pWriter.Write([]byte("<h1>404</h1> <p>Not Found"))
+    }
+
+    pWriter.Write(fileContent)
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    http.HandleFunc("/", staticFiles)
+
+    error := http.ListenAndServe("127.0.0.1:3000", nil)
+    if error != nil {
+        fmt.Fprintf(os.Stderr, "[ERROR]: The server encountered an error: %s", error)
+    }
 }
