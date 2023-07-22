f8c85425d1e255c6e4f7f22d8e352d24f453eefa
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-22 18:44:39 -0400

    Made the button submit data to the back-end and return to the main page.

--- frontend/index.js
+++ frontend/index.js
@@ -7,18 +7,7 @@ async function getCards() {
     return response
 }
 
-function main() {
-    let createStackPart = document.getElementById("create-stack-part")
-    let mainPage = document.getElementById("main")
-
-    createStackPart.remove()
-
-    let createStackButton = document.getElementById("button-create-stacks")
-    createStackButton.onclick = () => {
-        mainPage.parentElement.append(createStackPart)
-        mainPage.remove()
-    }
-
+function initMainPage() {
     getCards().then(cards => {
         let stacksListElement = document.getElementById("stack-list")
 
@@ -38,6 +27,47 @@ function main() {
             stacksListElement.appendChild(button)
         })
     })
+}
+
+function initCreateStackPage(mainPage, createStackPage) {
+    let button = document.getElementById("create-stack-submit-button")
+    button.onclick = () => {
+        let name = document.getElementById("new-stack-name").value
+        let description = document.getElementById("new-stack-description").value
+
+        const payload = {
+            name: name,
+            description: description
+        }
+
+        fetch("/api/cardstacks", {
+            method: "POST", 
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(payload)
+        }).then(() => {
+            createStackPage.parentElement.append(mainPage)
+            createStackPage.remove()
+            initMainPage()
+        }).catch(console.error)
+    }
+}
+
+function main() {
+    let createStackPart = document.getElementById("create-stack-part")
+    let mainPage = document.getElementById("main")
+
+    createStackPart.remove()
+
+    let createStackButton = document.getElementById("button-create-stacks")
+    createStackButton.onclick = () => {
+        mainPage.parentElement.append(createStackPart)
+        mainPage.remove()
+        initCreateStackPage(mainPage, createStackPart)
+    }
+
+    initMainPage()
 }
 
 main()
