7b5c52e37ab5ef6d9a2a8b7f07b504e4c540ac1c
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-02 12:36:54 -0400

    Filled in the empty elements.

--- frontend/stack.js
+++ frontend/stack.js
@@ -1,6 +1,17 @@
+async function getStack() {
+    const response = await fetch(`/api/cardstacks/${serverInfo.stackName}`)
+    const stack = response.json()
+    return stack
+}
+
 function main() {
-    let mainTitle = document.getElementById("main-title")
-    mainTitle.innerText = serverInfo.stackName
+    getStack().then((stack) => {
+        let mainTitle = document.getElementById("main-title")
+        mainTitle.innerText = stack.name
+
+        let mainDescription = document.getElementById("main-description")
+        mainDescription.innerText = stack.description
+    })
 }
 
 main()
