de7bec329c05dfbcbd6eaf2a5365ca3c7798895a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-04 14:51:55 -0400

    Added a page for managing the cards. Also cached the stack instead of reloading it all the time.

--- frontend/stack.js
+++ frontend/stack.js
@@ -1,17 +1,33 @@
+let globalStack;
+
+let mainPage;
+let cardsPage;
+
 async function getStack() {
     const response = await fetch(`/api/cardstacks/${serverInfo.stackName}`)
     const stack = response.json()
     return stack
 }
 
-function main() {
+function initMainPage() {
     getStack().then((stack) => {
+        globalStack = stack
+
         let mainTitle = document.getElementById("main-title")
         mainTitle.innerText = stack.name
 
         let mainDescription = document.getElementById("main-description")
         mainDescription.innerText = stack.description
     })
+}
+
+function main() {
+    mainPage = document.getElementById("main")
+    cardsPage = document.getElementById("cards-page")
+
+    cardsPage.parentElement.removeChild(cardsPage)
+
+    initMainPage()
 }
 
 main()
